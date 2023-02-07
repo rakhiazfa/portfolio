11bec97fb5550078fbb3eb3ec84bf3e6b7b27d2f
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-07 22:06:36 +0700

    Slicing skill section - About Page.

--- src/components/About/Sections/Skill.jsx
+++ src/components/About/Sections/Skill.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 
+const skills = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "PHP",
+    "Laravel",
+    "React JS",
+    "Bootstrap",
+    "Tailwind CSS",
+];
+
 const Skill = () => {
     return (
-        <section className="overflow-x-hidden">
+        <section className="overflow-x-hidden pb-20">
             <div className="wrapper">
                 <h1
                     className="text-4xl lg:text-5xl text-orange-400 font-bold tracking-wider mb-24"
@@ -10,6 +21,23 @@ const Skill = () => {
                 >
                     Skills
                 </h1>
+
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
+                    {skills?.map((skill, index) => (
+                        <div
+                            className="
+                                bg-gray-800 bg-opacity-50 rounded-lg px-5 py-3
+                            "
+                            key={index}
+                            data-aos="zoom-in"
+                            data-aos-duration={250 * (index + 1)}
+                        >
+                            <p className="text-lg font-medium text-center">
+                                {skill}
+                            </p>
+                        </div>
+                    ))}
+                </div>
             </div>
         </section>
     );
