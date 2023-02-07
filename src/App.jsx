7a925af6d3bb071632be209c2baebaddf9219959
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-07 14:58:04 +0700

    Setup aos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Router from "./router";
+import "aos/dist/aos.css";
+import AOS from "aos";
 
 function App() {
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
     return <Router />;
 }
 
