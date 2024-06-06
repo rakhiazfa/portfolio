d64f6132e068c9d8f964d4b31baa3a06e2b06523
rakhi <rakhiazfa0421@gmail.com>
2024-06-06 19:27:22 +0700

    feat(globals): add aos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { Inter } from "next/font/google";
+import type { AppProps } from 'next/app';
+import { Inter } from 'next/font/google';
+import { useEffect } from 'react';
+import AOS from 'aos';
+import '@/styles/globals.css';
+import 'aos/dist/aos.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        AOS.init({
+            duration: 750,
+            easing: 'ease-in-out',
+            once: true
+        });
+    }, []);
+
     return (
         <div className={inter.className}>
             <Component {...pageProps} />
