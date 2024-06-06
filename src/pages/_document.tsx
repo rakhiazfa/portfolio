ee07d15482d273c1db771a8b26ef0db53be8aa2b
rakhi <rakhiazfa0421@gmail.com>
2024-06-06 21:02:01 +0700

    fix: warning (don't set title in _document.tsx)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
     return (
         <Html lang="en">
-            <Head>
-                <title>Rakhi Azfa Rifansya</title>
-            </Head>
+            <Head />
             <body>
                 <Main />
                 <NextScript />
