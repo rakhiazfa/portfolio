7dd2f2573481e3f09acb9fb0ac9f95d44e1fdf99
rakhi <rakhiazfa0421@gmail.com>
2024-06-08 16:36:27 +0700

    feat: change dark mode configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'tailwindcss';
 const { default: flattenColorPalette } = require('tailwindcss/lib/util/flattenColorPalette');
 
 const config: Config = {
+    darkMode: 'class',
     content: [
         './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
         './src/components/**/*.{js,ts,jsx,tsx,mdx}',
