604a39cc357e7a6ec5ebeabe762b4e4baf63c317
rakhi <rakhiazfa0421@gmail.com>
2024-06-09 10:27:46 +0700

    adjust top loader height

--- src/components/layouts/main-layout.tsx
+++ src/components/layouts/main-layout.tsx
@@ -5,7 +5,7 @@ import Footer from '../container/footer';
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <>
-            <NextTopLoader showSpinner={false} height={1} zIndex={15000} speed={350} color="#ea580c" />
+            <NextTopLoader showSpinner={false} height={2} zIndex={15000} speed={350} color="#ea580c" />
             <Topbar />
             <div className="mt-[75px]">{children}</div>
             <Footer />
