2ab8a2c601c67c1ca3aa4b6f5130a918acd2418e
rakhi <rakhiazfa0421@gmail.com>
2024-06-10 21:54:06 +0700

    adjust cursor style

--- src/components/ui/cursor.tsx
+++ src/components/ui/cursor.tsx
@@ -57,7 +57,7 @@ const Cursor = () => {
     return (
         <div
             ref={cursorFollower}
-            className="absolute z-[-1] top-0 left-0 blur-3xl brightness-125 contrast-75 drop-shadow-xl grayscale-0 hue-rotate-180 backdrop-blur-xl backdrop-brightness-125 backdrop-opacity-50 backdrop-saturate-125 backdrop-sepia"
+            className="absolute z-[-1] top-0 left-0 opacity-50 blur-3xl brightness-125 contrast-75 drop-shadow-xl grayscale-0 hue-rotate-180 backdrop-blur-xl backdrop-brightness-125 backdrop-opacity-25 backdrop-saturate-125 backdrop-sepia"
         >
             <div className="pointer-events-none fixed left-0 top-0 aspect-square w-[450px] -translate-x-24 -translate-y-5 overflow-hidden rounded-full will-change-transform">
                 <div className="h-full w-full animate-spin-slow bg-gradient-to-r from-cyan-800 to-amber-700"></div>
