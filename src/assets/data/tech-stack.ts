9a11513f94dcf2a596c2ba74116fd7f6f431e075
rakhi <rakhiazfa0421@gmail.com>
2024-06-10 21:54:14 +0700

    adjust about section

--- src/assets/data/tech-stack.ts
+++ src/assets/data/tech-stack.ts
@@ -4,10 +4,10 @@ const techStack: {
     backend: string[];
     etc: string[];
 } = {
-    languages: ['PHP', 'Javascript', 'Typescript'],
-    frontend: ['HTML/CSS', 'React JS', 'Next JS', 'Vue JS', 'Tailwind CSS'],
-    backend: ['Laravel', 'Express JS', 'Nest JS', 'MySQL'],
-    etc: ['Docker', 'Object-oriented Programming', 'Rest API', 'Microservices']
+    languages: ['Javascript', 'Typescript', 'PHP'],
+    frontend: ['React JS', 'Next JS', 'Vue JS', 'Tailwind CSS'],
+    backend: ['Laravel', 'Express JS', 'Nest JS', 'MySQL', 'Redis'],
+    etc: ['Docker', 'Rest API', 'Microservices']
 };
 
 export default techStack;
--- src/components/container/sections/about-section.tsx
+++ src/components/container/sections/about-section.tsx
@@ -37,47 +37,60 @@ const AboutSection = () => {
                         Tech Stack
                     </h1>
                 </motion.div>
-                <motion.div
-                    initial={{ x: -50, opacity: 0 }}
-                    whileInView={{ x: 0, opacity: 1 }}
-                    transition={{ duration: 0.85 }}
-                    viewport={{ once: true }}
-                >
-                    <div className="grid grid-cols-1 gap-10">
-                        <div className="border-b border-semi-dark pb-5">
-                            <h3 className="text-base mb-5">Front-End Development</h3>
-                            <div className="flex items-center gap-5">
-                                {techStack.frontend.map((item, index) => (
-                                    <div key={index} className="bg-semi-dark rounded-full px-3 py-1">
-                                        <span className="text-xs tracking-wider">{item}</span>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                        <div className="border-b border-semi-dark pb-5">
-                            <h3 className="text-base mb-5">Back-End Development</h3>
-                            <div className="flex items-center gap-5">
-                                {techStack.backend.map((item, index) => (
-                                    <div key={index} className="bg-semi-dark rounded-full px-3 py-1">
-                                        <span className="text-xs tracking-wider">{item}</span>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                        <div className="border-b border-semi-dark pb-5">
-                            <h3 className="text-base mb-5">Others</h3>
-                            <div className="flex items-center gap-5">
-                                {techStack.etc.map((item, index) => (
-                                    <div key={index} className="bg-semi-dark rounded-full px-3 py-1">
-                                        <span className="text-xs tracking-wider">{item}</span>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </motion.div>
+                <div className="grid grid-cols-1 xl:grid-cols-2 gap-10">
+                    <motion.div
+                        initial={{ x: -50, opacity: 0 }}
+                        whileInView={{ x: 0, opacity: 1 }}
+                        transition={{ duration: 0.75 }}
+                        viewport={{ once: true }}
+                    >
+                        <TechStack title="Programming Languages" items={techStack.languages} />
+                    </motion.div>
+                    <motion.div
+                        initial={{ x: -50, opacity: 0 }}
+                        whileInView={{ x: 0, opacity: 1 }}
+                        transition={{ duration: 0.85 }}
+                        viewport={{ once: true }}
+                    >
+                        <TechStack title="Back-End Development" items={techStack.backend} />
+                    </motion.div>
+                    <motion.div
+                        initial={{ x: -50, opacity: 0 }}
+                        whileInView={{ x: 0, opacity: 1 }}
+                        transition={{ duration: 0.95 }}
+                        viewport={{ once: true }}
+                    >
+                        <TechStack title="Front-End Development" items={techStack.frontend} />
+                    </motion.div>
+                    <motion.div
+                        initial={{ x: -50, opacity: 0 }}
+                        whileInView={{ x: 0, opacity: 1 }}
+                        transition={{ duration: 1.05 }}
+                        viewport={{ once: true }}
+                    >
+                        <TechStack title="Others" items={techStack.etc} />
+                    </motion.div>
+                </div>
             </div>
         </section>
+    );
+};
+
+const TechStack = ({ title, items }: { title: string; items: string[] }) => {
+    return (
+        <div className="pb-3">
+            <h3 className="text-base mb-5">{title}</h3>
+            <div className="w-full flex flex-wrap items-center gap-5">
+                {items.map((item, index) => (
+                    <div
+                        key={index}
+                        className="bg-gradient-to-br from-blue-900 to-amber-900 rounded-full transform hover:scale-105 transition-all duration-300 px-3 py-1"
+                    >
+                        <span className="text-xs tracking-wider whitespace-nowrap">{item}</span>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 };
 
