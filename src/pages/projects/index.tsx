7769d6ecbe5a214120e707db55f3656f87936d53
rakhi <rakhiazfa0421@gmail.com>
2024-06-15 11:08:13 +0700

    adjust project page

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -2,34 +2,48 @@ import { database, storage } from '@/services/firebase';
 import { get, ref as databaseRef } from 'firebase/database';
 import { getDownloadURL, ref as storageRef } from 'firebase/storage';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 export default function Projects({ projects }: { projects: any[] }) {
-    console.log(projects);
     return (
         <main>
             <section id="project" className="w-full py-[5rem]">
                 <div className="app-container">
-                    <h1 className="max-w-[475px] text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold tracking-wide !leading-[1.35] mb-7">
-                        Projects
-                    </h1>
+                    <motion.div
+                        initial={{ x: -50, opacity: 0 }}
+                        whileInView={{ x: 0, opacity: 1 }}
+                        transition={{ duration: 0.85 }}
+                        viewport={{ once: true }}
+                    >
+                        <h1 className="max-w-[475px] text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold tracking-wide !leading-[1.35] mb-10">
+                            Projects
+                        </h1>
+                    </motion.div>
                     <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-10">
                         {projects.map((project, index) => (
-                            <div key={index} className="w-full">
-                                <div className="relative w-full aspect-[4/2] bg-gray-300 rounded-lg">
-                                    <Image
-                                        src={project.image}
-                                        width={0}
-                                        height={0}
-                                        sizes="100vw"
-                                        className="w-full h-auto object-cover rounded-lg"
-                                        alt={project.title}
-                                    />
+                            <motion.div
+                                key={index}
+                                initial={{ y: 75, opacity: 0 }}
+                                whileInView={{ y: 0, opacity: 1 }}
+                                transition={{ duration: 0.5 * (index + 1) }}
+                                viewport={{ once: true }}
+                            >
+                                <div className="w-full">
+                                    <div className="relative w-full aspect-[4/2] bg-gray-300 rounded-lg">
+                                        <Image
+                                            src={project.image}
+                                            width={0}
+                                            height={0}
+                                            sizes="100vw"
+                                            className="w-full h-auto object-cover rounded-lg"
+                                            alt={project.title}
+                                        />
+                                    </div>
+                                    <div className="px-3 py-5">
+                                        <h1 className="text-2xl font-bold tracking-wider mb-3">{project.title}</h1>
+                                    </div>
                                 </div>
-                                <div className="px-3 py-5">
-                                    <h1 className="text-sm font-semibold tracking-wider mb-3">{project.title}</h1>
-                                    <p className="text-gray-300">{project.description}</p>
-                                </div>
-                            </div>
+                            </motion.div>
                         ))}
                     </div>
                 </div>
@@ -38,7 +52,7 @@ export default function Projects({ projects }: { projects: any[] }) {
     );
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
     const snapshot = await get(databaseRef(database, 'projects'));
     const data = snapshot.exists() ? snapshot.val() : {};
     const projects = await Promise.all(
